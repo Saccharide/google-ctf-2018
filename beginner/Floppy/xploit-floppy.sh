ae88f7cfb5f6964b03f00488947c0f2fe29a5c91
saccharide <rtang@email.wm.edu>
2018-07-14 00:36:31 -0400

    Added explanation for solution.

--- beginner/Floppy/xploit-floppy.sh
+++ beginner/Floppy/xploit-floppy.sh
@@ -1,5 +1,5 @@
 
-
+# After using binwalk, a tool that tries to see if the file passed in is actually another type of file, I found out that foo.ico is actually a zip file, so let's extract it. And we see that the flag is in driver.txt.
 cp foo.ico foo.zip
 unzip -q foo.zip
 
