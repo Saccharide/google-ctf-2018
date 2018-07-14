4b45894e2b23c0830941232a15b580eeb70e7039
saccharide <rtang@email.wm.edu>
2018-07-14 00:12:19 -0400

    Added explanation for my solution.

--- beginner/Letter/xploit-letter.sh
+++ beginner/Letter/xploit-letter.sh
@@ -1,5 +1,6 @@
 
-
+# Using linux's pdftotext to convet the PDF file to normal text file to see the content of the email. The black rectangle box is actually just a layer on top of the actual page, so we can still see the underneath password filed.
 pdftotext challenge.pdf
 
+# Using grep to find the flag
 echo `grep -o "CTF{.*}" challenge.txt`
